4a8a2cfb03a2de41d82886ef569dbf214a0cbf01
uzs777 <uzsstore777@gmail.com>
2025-07-30 14:02:31 +0500

    fvfe

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const product = require("./router/products");
+const cors = require("cors")
 
 mongoose
     .connect("mongodb+srv://kkaxxarovv:hIQLkT2k0G0IJ5YV@cluster0.u7pokh7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -9,8 +10,10 @@ mongoose
 
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/products", product);
 
-app.listen(3000, () => { console.log("server started on port 3000") })
+const port = process.env.PORT || 3000
+app.listen(port, () => { console.log(`server started on port ${port}`) })
